46d471382602fd15cd2b544c82303aa783203ddc
mickeymond <33749130+mickeymond@users.noreply.github.com>
2024-03-18 19:48:33 +0000

    Making calls from todo-app

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 import todosRoutes from "./routes/todos.routes.js";
 
 // create express app
@@ -8,6 +9,7 @@ const app = express();
 // Apply middlewares
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 // Use routes
 app.use(todosRoutes);
--- routes/todos.routes.js
+++ routes/todos.routes.js
@@ -1,18 +1,66 @@
 import { Router } from "express";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config({ path: ['.env.local'] });
 
 const router = Router();
 
+const url = process.env.MONGO_URI;
+
+const client = new MongoClient(url);
+const todoDb = 'todo-db';
+const todoCollection = 'todos';
+
 // Define routes
-router.post('/todos', (req, res) => {
-    res.json(req.body);
+router.post('/todos', async (req, res) => {
+    // Connect mongodb client
+    await client.connect();
+    // Get access to todo database
+    const db = client.db(todoDb);
+    // Get access to todos collection
+    const collection = db.collection(todoCollection);
+    // Add todo document to todos collection
+    const insertOneResult = await collection.insertOne({
+        ...req.body,
+        isCompleted: false,
+        createdAt: new Date()
+    });
+    // Disconnect mongodb client
+    await client.close();
+    // Return reponse
+    res.json(insertOneResult);
 });
 
-router.get('/todos', (req, res) => {
-    res.send('Get all todos!');
+router.get('/todos', async (req, res) => {
+    // Connect mongodb client
+    await client.connect();
+    // Get access to todo database
+    const db = client.db(todoDb);
+    // Get access to todos collection
+    const collection = db.collection(todoCollection);
+    // Find all todos from todos collection
+    const limit = parseInt(req.query.limit) || 10;
+    const findResult = await collection.find({}).limit(limit).toArray();
+    // Disconnect mongodb client
+    await client.close();
+    // Return reponse
+    res.json(findResult);
 });
 
-router.delete('/todos', (req, res) => {
-    res.send('Delete all todos!');
+router.delete('/todos', async (req, res) => {
+    // Connect mongodb client
+    await client.connect();
+    // Get access to todo database
+    const db = client.db(todoDb);
+    // Get access to todos collection
+    const collection = db.collection(todoCollection);
+    // Delete all todos from todos collection
+    const deleteManyResult = await collection.deleteMany({});
+    // Disconnect mongodb client
+    await client.close();
+    // Return reponse
+    res.json(deleteManyResult);
 });
 
 router.get('/todos/:id', (req, res) => {
